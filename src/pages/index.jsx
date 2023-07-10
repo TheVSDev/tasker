a29b523564a68bfd5ef12baee0f87350527cc08e
TheVSDev <vspasic03@gmail.com>
2023-07-11 00:10:36 +0200

    done: welcome page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,6 +2,8 @@
 import Main from "@/web/components/Main";
 import Footer from "@/web/components/Footer";
 import HeadPage from "@/web/components/HeadPage";
+import Title from "@/web/components/Title";
+import styles from "@/web/styles/Welcome.module.css"
 
 // Home page function
 export default function Home() {
@@ -9,7 +11,9 @@ export default function Home() {
     <>
       <HeadPage />
       <Main>
-        <h1>Tasker</h1>
+        <div className={styles.container}></div>
+        <Title titleLabel="Tasker" />
+        <p className={styles.paragraphe}>Tasker is a web task manager. You can create different lists with different tasks, <br /> you can organize your calender with tasks and events and you can create quick day-to-day lists.</p>
       </Main>
       <Footer />
     </>
